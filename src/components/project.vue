908c9b2c9bd92f25263d3bfc4f2ff7d10bb16f3b
Yuju <ujusunrise@gmail.com>
2023-02-06 11:53:03 +0900

    23-02-06

--- src/components/project.vue
+++ src/components/project.vue
@@ -7,59 +7,62 @@
     <div class="pjt_right">
       <div class="pjt_cont_wrap">
         <div class="pjt_cont_propa" data-aos="fade-up" data-aos-delay="200"  data-aos-duration="700">
-          <div class="pjt_cont_img">
-            <img src="../assets/project/propaganda2.png" alt="">
+          <div class="pjt_cont_img propaImgbox">
+            <img src="../assets/project/propaganda2.png" class="propaImg" alt="">
           </div>
           <div class="pjt_cont_txt">
             <div class="pjt_txt_left">
               <h4>프로파간다 홈페이지 리뉴얼</h4>
-              <p>프로파간다는 시각 문화 전문지 'GRAPHIC'을 창간하기 위해 출범한 출판사로, 이제는 단행본까지 출판 범위를 확장하고 있습니다. 기존의 단순한 홈페이지를 출판사의 분위기를 살려 잡지 뿐만 아니라 다양한 도서들까지 한눈에 볼 수 있도록 하였습니다. </p>
+              <p>프로파간다는 시각 문화 전문지 'GRAPHIC' 과 다양한 단행본을 출간하는 출판사입니다. 기존의 단순한 레이아웃관 정보들이 생략된 홈페이지를 출판사의 분위기를 살려 잡지 뿐만 아니라 다양한 도서들까지 한눈에 볼 수 있으며 정보 제공에도 용이하게 리뉴얼 하였습니다. </p>
               <span class="pjt_txt_info">제작기간 : 2022.12.02 ~ 2022.12.24</span>
               <span>제작프로그램 : HTML, CSS, JS, NODE.JS 등</span>
+               <span class="pjt_txt_per">참여인원 : 1인</span>
             </div>
             <div class="pjt_txt_right">
               <div class="pjt_btn_page">
-                <a href="" class="pjt_btn btn_arrow">자세히보기</a></div>
-              <div class="pjt_btn_mockup"><a href="" class="pjt_btn btn_arrow">페이지리뷰</a></div>
-              <div class="pjt_btn_github"><a href="" class="pjt_btn btn_arrow">깃허브</a></div>
+                <a href="https://project-propa.herokuapp.com/" class="pjt_btn btn_arrow" target="_blank">자세히보기</a></div>
+              <div class="pjt_btn_mockup"><a href="https://velog.io/@ujusunrise/프로파간다-리뉴얼-코드리뷰" class="pjt_btn btn_arrow" target="_blank">페이지리뷰</a></div>
+              <div class="pjt_btn_github"><a href="https://github.com/ujumango/project-propaganda.git" class="pjt_btn btn_arrow" target="_blank">깃허브</a></div>
             </div>
           </div>
         </div>
         <div class="pjt_cont_re" data-aos="fade-up" data-aos-delay="300"  data-aos-duration="700">
-          <div class="pjt_cont_img">
-            <img src="../assets/project/rexre2.png" alt="">
+          <div class="pjt_cont_img propaImgbox">
+            <img src="../assets/project/rexre2.png" class="propaImg" alt="">
           </div>
           <div class="pjt_cont_txt">
             <div class="pjt_txt_left">
               <h4>리바이리 홈페이지 리뉴얼</h4>
-              <p>멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 길게길게길게 세줄 세줄 세줄 되나요 되나요 되나요? 됩니다 </p>
+              <p>리바이리는 천연원료 전문 기업인 현대바이오랜드에서 만든 비건 뷰티 브랜드입니다. 기존의 제품과 컨셉을 알기 어렵던 홈페이지를 브랜드 컨셉을 통일해주고 전보다 쉽고 빠르게 카테고리 별로 제품을 접할 수 있도록 리뉴얼 하였습니다.</p>
               <span class="pjt_txt_info">제작기간 : 2022.12.02 ~ 2022.12.24</span>
               <span>제작프로그램 : HTML, CSS, JS, NODE.JS 등</span>
+               <span class="pjt_txt_per">참여인원 : 1인</span>
             </div>
             <div class="pjt_txt_right">
               <div class="pjt_btn_page">
                 <a href="https://project-rexre.herokuapp.com/" class="pjt_btn btn_arrow" target="_blank">자세히보기</a></div>
-              <div class="pjt_btn_mockup"><a href="" class="pjt_btn btn_arrow">페이지리뷰</a></div>
-              <div class="pjt_btn_github"><a href="" class="pjt_btn btn_arrow">깃허브</a></div>
+              <div class="pjt_btn_mockup"><a href="" class="pjt_btn btn_arrow" target="_blank">페이지리뷰</a></div>
+              <div class="pjt_btn_github"><a href="https://github.com/ujumango/rexre.git" class="pjt_btn btn_arrow" target="_blank">깃허브</a></div>
             </div>
           </div>
         </div>
         <div class="pjt_cont_team"  data-aos="fade-up" data-aos-delay="300"  data-aos-duration="600">
-          <div class="pjt_cont_img">
-            <img src="../assets/project/team.png" alt="">
+          <div class="pjt_cont_img propaImgbox">
+            <img src="../assets/project/team.png" class="propaImg" alt="">
           </div>
           <div class="pjt_cont_txt">
             <div class="pjt_txt_left">
               <h4>부산광역시 축제위원회 홈페이지 리뉴얼</h4>
-              <p>멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 멋지다 연진아 브라보 길게길게길게 세줄 세줄 세줄 되나요 되나요 되나요? 됩니다 </p>
+              <p> 부산광역시 축제위원회는 부산의 문화 축제를 소개하는 단체입니다. 기존의 축제 프로그램이 눈에 띄지 않고 컨셉이 명확하지 않았던 페이지를 블랙과 화이트를 기본으로 하고 포인트들을 주어 통통 튀는 축제 페이지로   리뉴얼 하였습니다.</p>
               <span class="pjt_txt_info">제작기간 : 2022.12.02 ~ 2022.12.24</span>
               <span>제작프로그램 : HTML, CSS, JS, NODE.JS 등</span>
+               <span class="pjt_txt_per">참여인원 : 3인</span>
             </div>
             <div class="pjt_txt_right">
               <div class="pjt_btn_page">
                 <a href="https://project-busan.herokuapp.com/" class="pjt_btn btn_arrow" target="_blank">자세히보기</a></div>
-              <div class="pjt_btn_mockup"><a href="" class="pjt_btn btn_arrow">페이지리뷰</a></div>
-              <div class="pjt_btn_github"><a href="" class="pjt_btn btn_arrow">깃허브</a></div>
+              <div class="pjt_btn_mockup"><a href="" class="pjt_btn btn_arrow" target="_blank">페이지리뷰</a></div>
+              <div class="pjt_btn_github"><a href="https://github.com/ujumango/busanfestival_team.git" class="pjt_btn btn_arrow" target="_blank">깃허브</a></div>
             </div>
           </div>
         </div>
@@ -67,11 +70,11 @@
       </div>
     </div>
   </div>
-  <div class="project_bnr">
+  <!-- <div class="project_bnr">
     <div class="bjt_bnr_wrap">
       <span class="h1txt">[</span><h1 class="typingW underline"></h1><span class="h1txt ">]</span><h1 class="normalTxt">하는</h1> <h1>프론트엔드</h1><h1 class="normalTxt">개발자</h1>
     </div>
-  </div>
+  </div> -->
 </section>
  
 </template>
@@ -96,31 +99,56 @@ export default {
       
         }
 
-      const letters = ['도전','발전','노력'];
-      let i = 0;
-      const letter = letters[i].split('');
-      const targets = document.querySelector('.typingW')
-      // console.log(targets)
-      console.log(letters[0])
+      // const letters = ['도전','발전','노력'];
+      // let i = 0;
+      // const letter = letters[i].split('');
+      // const targets = document.querySelector('.typingW')
+      // // console.log(targets)
+      // console.log(letters[0])
 
-      function typing(arr){
-        if(arr.length > 0){
-          targets.textContent += arr.shift();
-          setTimeout(function() {
-            typing(arr)}, 150)
-        }
-      }
-       typing(letter);
+      // function typing(arr){
+      //   if(arr.length > 0){
+      //     targets.textContent += arr.shift();
+      //     setTimeout(function() {
+      //       typing(arr)}, 150)
+      //   }
+      // }
+      //  typing(letter);
        
 
-        function blink(){
-        targets.classList.toggle('active');
-        }
-        setInterval(blink, 500) //blink 함수를 0.5초마다 실행
+      //   function blink(){
+      //   targets.classList.toggle('active');
+      //   }
+      //   setInterval(blink, 500) //blink 함수를 0.5초마다 실행
       
 
+
+       let projectImgbox = document.querySelectorAll('.propaImgbox')
+       let projectImg = document.querySelectorAll('.propaImg')
+
+       console.log('이미지박스' + projectImgbox)
+       console.log('이미지' + projectImg)
+
+       for(let i=0; i<projectImgbox.length; i++){
+        projectImg[i].addEventListener('mouseover', ()=>{
+        projectImg[i].style.opacity = "0";
+        projectImg[i].style.transition = `all 0.5s`
+        projectImgbox[i].classList.add('hover')
+        
+      })
+           projectImg[i].addEventListener('mouseleave', ()=>{
+        projectImg[i].style.opacity = "1";
+        projectImg[i].style.transition = `all 0.5s`
+        projectImgbox[i].classList.remove('hover')
+        
+      })
+       }
+
+ 
+ 
        })
 
+   
       //  dynamic(split);
 
 
@@ -171,7 +199,14 @@ export default {
 }
 #project .pjt_cont_wrap .pjt_cont_propa .pjt_cont_img{
   background-image: url(../assets/project/propagandaBg.png);
+  transition:all 0.3s ease-in-out
 }
+#project .pjt_cont_wrap .pjt_cont_propa .pjt_cont_img.hover{
+  background-image: url(../assets/project/propa_full.gif);
+  filter: brightness(0.8);
+  transition:all 0.3s ease-in-out
+}
+
 #project .pjt_cont_wrap  .pjt_cont_img img{
   display: block;
   width: 65%;
@@ -241,6 +276,10 @@ export default {
   display: block;
   padding-top: 20px;
 }
+ .pjt_txt_per{
+  display: block;
+  /* padding-top: 20px; */
+}
 
  .btn_arrow{
   width:100%;
@@ -294,8 +333,18 @@ background-image: url(../assets/common/resumeBk.png);
 #project .pjt_cont_wrap .pjt_cont_re .pjt_cont_img{
   background-image: url(../assets/project/rexreBg.png);
 }
+#project .pjt_cont_wrap .pjt_cont_re .pjt_cont_img.hover{
+  background-image: url(../assets/project/rexre-full.gif);
+  filter: brightness(0.8);
+  transition:all 0.3s ease-in-out
+}
 #project .pjt_cont_wrap .pjt_cont_team .pjt_cont_img{
   background-image: url(../assets/project/teamBg.png);
+}
+#project .pjt_cont_wrap .pjt_cont_team .pjt_cont_img.hover{
+  background-image: url(../assets/project/busan-full.gif);
+  filter: brightness(0.8);
+  transition:all 0.3s ease-in-out
 }
 /* #project .pjt_cont_wrap .pjt_cont_dusan{
   padding-bottom: 18%;
